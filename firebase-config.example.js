e8349817ca9788cc05c327cd06ac443437ef20e9
Dlaize Kohli <dakshkohli23@gmail.com>
2026-04-10 14:50:55 +0530

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.example.js  ← COMMIT THIS
-export const firebaseConfig = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId:             "YOUR_APP_ID"
-};
