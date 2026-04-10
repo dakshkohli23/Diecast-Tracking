3a70efa7b68b52a8c8c3a28fc70cc21a57b1e34b
Dlaize Kohli <dakshkohli23@gmail.com>
2026-04-10 14:48:11 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js  ← DO NOT COMMIT THIS FILE
+export const firebaseConfig = {
+  apiKey:            "AIzaSyA2-6u8rETOIn9xUJQW0ZODFupZQ56orJg",
+  authDomain:        "diecast-tracking-471f7.firebaseapp.com",
+  projectId:         "diecast-tracking-471f7",
+  storageBucket:     "diecast-tracking-471f7.firebasestorage.app",
+  messagingSenderId: "1042711268055",
+  appId:             "1:1042711268055:web:9b09ded970a85532767e92"
+};
